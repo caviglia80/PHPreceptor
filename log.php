6e6dd50f34dd3e4a6905ea5a290a00aa51131486
Francisco <caviglia80@gmail.com>
2024-02-29 15:25:28 +0100

    .

--- log.php
+++ log.php
@@ -1,11 +1,11 @@
 <?php
 $fields = [
-	'accelerometerX', 
-	'accelerometerY', 
-	'accelerometerZ', 
-	'gyroX', 
-	'gyroY', 
-	'gyroZ', 
+	'accelerometerX',
+	'accelerometerY',
+	'accelerometerZ',
+	'gyroX',
+	'gyroY',
+	'gyroZ',
 	'temperature',
 	'Kp',
 	'Ki',
@@ -20,10 +20,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		$response[$field] = isset($_POST[$field]) ? $_POST[$field] : 'No data';
 	}
 
-    // Add a timestamp
+	// Add a timestamp
 	$response['timestamp'] = time();
 
-    // Save data to a JSON file
+	// Save data to a JSON file
 	file_put_contents('data.json', json_encode($response));
 }
 
@@ -31,19 +31,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 	header('Content-Type: application/json');
 
-    // Check if file exists and return its content
+	// Check if file exists and return its content
 	if (file_exists('data.json')) {
 		$contents = file_get_contents('data.json');
 		$data = json_decode($contents, true);
 
-        // Check if more than 4 seconds have passed
+		// Check if more than 4 seconds have passed
 		if (time() - $data['timestamp'] > 5) {
 			$response = array();
 			foreach ($fields as $field) {
 				$response[$field] = 'No data';
 			}
 
-            // Add a timestamp
+			// Add a timestamp
 			$response['timestamp'] = time();
 
 			echo json_encode($response);
@@ -54,4 +54,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 		echo json_encode(['error' => 'No data available']);
 	}
 }
-?>
+
